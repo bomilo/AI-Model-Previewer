37dcd9e7fe21515ac8b61f1f18a39c1911eb1b3b
Bosko Milosavljevic <bosko.milosavljevic@marvelsoft.net>
2017-11-15 09:34:18 +0100

    ChartingApp - Initial commit

---
+++ ChartingApp/ChartingApp/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace ChartingApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cartesianChart1.BackColor = System.Drawing.Color.Black;
+            this.cartesianChart1.Location = new System.Drawing.Point(2, 88);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(735, 408);
+            this.cartesianChart1.TabIndex = 0;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.cartesianChart1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(2);
+            this.panel1.Size = new System.Drawing.Size(739, 498);
+            this.panel1.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Black;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.label7.Location = new System.Drawing.Point(615, 135);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 1;
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Black;
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.label6.Location = new System.Drawing.Point(606, 109);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 0;
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.comboBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(2, 2);
+            this.panel2.Margin = new System.Windows.Forms.Padding(1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(739, 87);
+            this.panel2.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(8, 7);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Instrument info";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(571, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 32);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkGray;
+            this.label3.Location = new System.Drawing.Point(412, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 15);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Instrument Description";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.MintCream;
+            this.label1.Location = new System.Drawing.Point(9, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Type:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox1.Location = new System.Drawing.Point(260, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(134, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.TextBox1_KeyUp);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.MintCream;
+            this.label2.Location = new System.Drawing.Point(206, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Name:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ItemHeight = 13;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Shares",
+            "Bonds",
+            "ETFs & Funds",
+            "Indices"});
+            this.comboBox1.Location = new System.Drawing.Point(58, 39);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 502);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(759, 541);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(2);
+            this.Text = "TradeZmarter Prototype App";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ ChartingApp/ChartingApp/Form1.cs
@@ -0,0 +1,359 @@
+﻿using LiveCharts;
+using LiveCharts.Configurations;
+using LiveCharts.Wpf;
+using System;
+using System.Windows.Forms;
+using System.Windows.Media;
+using System.Drawing.Text;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
+namespace ChartingApp
+{
+
+    public partial class Form1 : Form
+    {
+        [DllImport("gdi32.dll")]
+        private static extern IntPtr AddFontMemResourceEx(IntPtr pbfont, uint cbfont, IntPtr pdv, [In] ref uint pcFonts);
+
+        System.Drawing.FontFamily ff;
+        System.Drawing.Font font;
+
+        int predictionCounter = 0;
+        int realMarketCounter = 0;
+        //private static double previousPoint = 0;
+
+        public ChartValues<MeasureModel> PredictionValues { get; set; }
+        public ChartValues<MeasureModel> RealMarketValues { get; set; }
+        public Timer PredictionTimer { get; set; }
+        public Timer RealMarketTimer { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            var mapper = Mappers.Xy<MeasureModel>()
+                .X(model => model.DateTime.Ticks)   //use DateTime.Ticks as X
+                .Y(model => model.Value);           //use the value property as Y
+
+            //Save the mapper globally.
+            Charting.For<MeasureModel>(mapper);
+
+            //the ChartValues property will store our values array
+            PredictionValues = new ChartValues<MeasureModel>();
+            RealMarketValues = new ChartValues<MeasureModel>();
+
+            Brush BrushStrokePrediction = Brushes.MediumVioletRed;
+            Brush BrushFillPrediction = Brushes.Transparent;
+
+            //Brush BrushFillReal = new SolidColorBrush(Color.FromArgb(100, 159, 223, 188));
+            Brush BrushFillReal = Brushes.Transparent;
+            Brush BrushStrokeReal = Brushes.DarkGreen;
+
+            cartesianChart1.Series = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Values = PredictionValues,
+                    LineSmoothness = 0,
+                    PointGeometrySize = 6,
+                    Stroke = BrushStrokePrediction,
+                    StrokeThickness = 2,
+                    Title = "Prediction",
+                    Fill = BrushFillPrediction,
+
+                },
+                new LineSeries
+                {
+                    Values = RealMarketValues,
+                    LineSmoothness = 0,
+                    PointGeometrySize = 6,
+                    StrokeThickness = 2,
+                    Title = "RealMarket",
+                    Stroke = BrushStrokeReal,
+                    Fill = BrushFillReal
+                },
+
+            };
+
+            FontFamily family = new FontFamily("Ubuntu Light");
+
+            cartesianChart1.AxisX.Add(new Axis
+            {
+                Title = "Time",
+                FontSize = 12,
+                FontFamily = family,
+                DisableAnimations = false,
+                LabelFormatter = value => new DateTime((long)value).ToString("HH:mm:ss:fff"),
+                LabelsRotation = -45,
+                Visibility = System.Windows.Visibility.Hidden,
+                MinValue = DataHolder.PredictionTimes[0].Ticks - new TimeSpan(0, 2, 0).Ticks,
+                MaxValue = DataHolder.PredictionTimes[DataHolder.PredictionTimes.Length - 1].Ticks + new TimeSpan(0, 2, 0).Ticks,
+                Separator = new Separator
+                {
+                    Step = TimeSpan.FromSeconds(30).Ticks,
+                    StrokeThickness = 0.0,
+                }
+            });
+
+            cartesianChart1.AxisY.Add(new Axis
+            {
+                Title = "Price",
+                FontSize = 12,
+                FontFamily = family,
+                ShowLabels = true,
+                IsMerged = true,
+                MaxValue = 479.0000,
+                MinValue = 477.0000,
+                Separator = new Separator
+                {
+                    StrokeThickness = 0.1,
+                    StrokeDashArray = new DoubleCollection(new double[] { 4 }),
+                    Stroke = new SolidColorBrush(Color.FromRgb(64, 79, 86)),
+                }
+            });
+
+            cartesianChart1.Zoom = ZoomingOptions.Xy;
+        }
+
+        #region Font Related Stuff
+
+        private void LoadFont()
+        {
+            byte[] fontAray = Properties.Resources.Ubuntu_L;
+            int dataLenght = Properties.Resources.Ubuntu_L.Length;
+            IntPtr ptrData = Marshal.AllocCoTaskMem(dataLenght);
+
+            Marshal.Copy(fontAray, 0, ptrData, dataLenght);
+
+            uint cFonts = 0;
+
+            AddFontMemResourceEx(ptrData, (uint)fontAray.Length, IntPtr.Zero, ref cFonts);
+
+            PrivateFontCollection pfc = new PrivateFontCollection();
+            pfc.AddMemoryFont(ptrData, dataLenght);
+
+            Marshal.FreeCoTaskMem(ptrData);
+
+            ff = pfc.Families[0];
+            font = new System.Drawing.Font(ff, 15f, System.Drawing.FontStyle.Bold);
+
+        }
+
+        private void AllocFont(System.Drawing.Font f, Control c, float size, System.Drawing.FontStyle fontStyle = System.Drawing.FontStyle.Regular)
+        {
+            c.Font = new System.Drawing.Font(ff, size, fontStyle);
+        }
+
+        #endregion
+
+        private void SetAxisLimits(DateTime time)
+        {
+            //cartesianChart1.AxisX[0].MaxValue = time.Ticks + TimeSpan.FromSeconds(1).Ticks; // lets force the axis to be 100ms ahead
+            //cartesianChart1.AxisX[0].MinValue = time.Ticks - TimeSpan.FromSeconds(480).Ticks; //we only care about the last 8 seconds
+        }
+
+        public string GetInstrumentDecription(string instrument)
+        {
+            switch (instrument)
+            {
+                case "STMN":
+                    {
+                        return "Straumann Holding AG";
+                    }
+                default:
+                    {
+                        return "";
+                    }
+            }
+        }
+
+        private void PredictionTimerOnTickHandler(object sender, EventArgs eventArgs)
+        {
+
+            if (predictionCounter > DataHolder.PredictionPrices.Length - 1)
+            {
+                PredictionTimer.Stop();
+                return;
+            }
+
+            AddNewPoint(DataHolder.PredictionTimes, DataHolder.PredictionPrices, PredictionValues, predictionCounter);
+            predictionCounter++;
+
+            //lets only use the last 30 values
+            //if (PredictionValues.Count > 30) PredictionValues.RemoveAt(0);
+        }
+
+        private void AddNewPoint(DateTime[] times, double[] prices, ChartValues<MeasureModel> destinationLSeriePointcollection, int counter)
+        {
+            MeasureModel Point = new MeasureModel();
+
+            Point.DateTime = times[counter];
+            Point.Value = ReturnNormalizedPredictedPrice(prices, counter);
+            destinationLSeriePointcollection.Add(Point);
+
+            System.Diagnostics.Debug.WriteLine(Point.Value);
+        }
+
+        private void MarketSimulationExecutionTimerOnTickHandler(object sender, EventArgs e)
+        {
+            if (realMarketCounter > DataHolder.RealPrices.Length - 1)
+            {
+                RealMarketTimer.Stop();
+                return;
+            }
+
+            AddNewPoint(DataHolder.RealTimes, DataHolder.RealPrices, RealMarketValues, realMarketCounter);
+            realMarketCounter++;
+
+
+            //lets only use the last 30 values
+            //if (RealMarketValues.Count > 30) RealMarketValues.RemoveAt(0);
+        }
+
+        private double ReturnNormalizedPredictedPrice (double[] predictedPrice, int index)
+        {
+            double calculatedPrice = 0;
+            double tickSize = DataHolder.TickTable[textBox1.Text];
+            double previousPrice = (index != 0) ? predictedPrice[index - 1] : 0;
+            double currentPrice = predictedPrice[index];
+
+            if (currentPrice > previousPrice)
+            {
+                calculatedPrice = Math.Floor(currentPrice / tickSize) * tickSize;
+            }
+            else if (currentPrice == previousPrice)
+            {
+                calculatedPrice = previousPrice;
+            }
+            else
+            {
+                calculatedPrice = Math.Ceiling(currentPrice / tickSize) * tickSize;
+            }
+            return calculatedPrice;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != String.Empty)
+            {
+                label6.Text = textBox1.Text;
+                label7.Text = "SWISS Exchange";
+
+                #region Prediction related
+
+                if (PredictionTimer != null)
+                {
+                    predictionCounter = 0;
+                    PredictionTimer.Stop();
+                    PredictionTimer.Dispose();
+                    PredictionTimer = null;
+                    cartesianChart1.Series[0].Values.Clear();
+                }
+
+                PredictionTimer = new Timer
+                {
+                    Interval = 750
+                };
+                PredictionTimer.Tick += PredictionTimerOnTickHandler;
+                PredictionTimer.Start();
+
+
+                #endregion
+
+                #region Market Execution Simulation
+
+                if (RealMarketTimer != null)
+                {
+                    realMarketCounter = 0;
+                    RealMarketTimer.Stop();
+                    RealMarketTimer.Dispose();
+                    RealMarketTimer = null;
+                    cartesianChart1.Series[1].Values.Clear();
+                }
+
+                RealMarketTimer = new Timer
+                {
+                    Interval = 1000
+                };
+                RealMarketTimer.Tick += MarketSimulationExecutionTimerOnTickHandler;
+                RealMarketTimer.Start();
+
+                #endregion
+
+                #region Quick Testing Code
+
+                //new System.Threading.Thread(() =>
+                //{
+
+
+                //    for (int i = 0; i < DataHolder.PredictionTimes.Length; i++)
+                //    {
+                //        MeasureModel PredictionPoint = new MeasureModel();
+                //        MeasureModel RealMarketPoint;
+                //        PredictionPoint.DateTime = DataHolder.PredictionTimes[predictionCounter];
+                //        PredictionPoint.Value = DataHolder.PredictionPrices[predictionCounter];
+                //        PredictionValues.Add(PredictionPoint);
+                //        predictionCounter++;
+
+                //        while (true)
+                //        {
+                //            if (realMarketCounter < DataHolder.RealTimes.Length)
+                //            {
+                //                RealMarketPoint = new MeasureModel();
+                //                RealMarketPoint.DateTime = DataHolder.RealTimes[realMarketCounter];
+                //                RealMarketPoint.Value = DataHolder.RealPrices[realMarketCounter];
+                //                RealMarketValues.Add(RealMarketPoint);
+                //                realMarketCounter++;
+
+                //                //System.Threading.Thread.Sleep(100);
+
+                //                if (realMarketCounter % 5 == 0)
+                //                {
+                //                    realMarketCounter++;
+                //                    break;
+                //                }
+                //            }
+                //            else
+                //            {
+                //                break;
+                //            }
+                //        }
+                //    }
+
+                //    predictionCounter = 0;
+                //    realMarketCounter = 0;
+
+                //}).Start();
+
+                #endregion
+            }
+        }
+
+        private void TextBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            label3.Text = GetInstrumentDecription(textBox1.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadFont();
+            AllocFont(font, this.label5, 12f, System.Drawing.FontStyle.Bold);
+            AllocFont(font, this.label5, 8f, System.Drawing.FontStyle.Italic);
+            AllocFont(font, this.label1, 10f);
+            AllocFont(font, this.label2, 10f);
+            AllocFont(font, this.label3, 8f);
+            AllocFont(font, this.button1, 10f);
+            AllocFont(font, this.comboBox1, 8f);
+            AllocFont(font, this.textBox1, 8f);
+            AllocFont(font, this.label6, 15f, System.Drawing.FontStyle.Bold);
+
+        }
+    }
+
+    public class MeasureModel
+    {
+        public DateTime DateTime { get; set; }
+        public double Value { get; set; }
+    }
+}
